73734d22e04ffe28f108cecacc06d6b1c7238460
kuang yi <1057709392@qq.com>
2024-03-11 11:00:59 +0800

    项目创建

---
+++ src/main/java/com/project/icpcwiki/IcpcWikiApplication.java
@@ -0,0 +1,13 @@
+package com.project.icpcwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcpcWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcpcWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/icpcwiki/IcpcWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.icpcwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcpcWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
