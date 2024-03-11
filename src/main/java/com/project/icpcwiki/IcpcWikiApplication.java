65fc37e9b4470f94a296345be4a95aa616e7f6d1
kuang yi <1057709392@qq.com>
2024-03-11 12:49:20 +0800

    修改启动类

--- src/main/java/com/project/icpcwiki/IcpcWikiApplication.java
+++ src/main/java/com/project/icpcwiki/IcpcWikiApplication.java
@@ -1,13 +1,22 @@
 package com.project.icpcwiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class IcpcWikiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(IcpcWikiApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(IcpcWikiApplication.class, args);
+        SpringApplication app = new SpringApplication(IcpcWikiApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+//        SpringApplication.run(IcpcWikiApplication.class, args);
+        LOG.info("启动成功！");
+        LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
