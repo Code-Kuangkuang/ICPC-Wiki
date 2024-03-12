58f8e9350b6f017437b47dbd049897e1ad5f4f38
kuang yi <1057709392@qq.com>
2024-03-12 10:38:57 +0800

    mybatis自动生成器及测试

--- doc/all.sql
+++ doc/all.sql
@@ -4,3 +4,14 @@ create table `test` (
     `name` varchar(50) comment '名称',
     primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='测试';
+
+
+# Demo表
+drop table if exists `demo`;
+create table `demo` (
+                        `id` bigint not null COMMENT 'id',
+                        `name` varchar(50) COMMENT '名称',
+                        `password` varchar(50) COMMENT '名称',
+                        primary key (`id`)
+) engine=InnoDB default charset=utf8mb4 comment= '测试';
+insert into `demo` (id, name) values (1,'测试');
